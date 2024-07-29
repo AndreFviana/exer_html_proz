c0dbd72fce63eea64c6dd258a56f37726baf7f49
Andre Viana <91017517+AndreFviana@users.noreply.github.com>
2024-07-29 15:57:41 -0300

    consumindo API e renderizando dados no DOM

---
+++ FetchApi/scriptFetchApi.js
@@ -0,0 +1,55 @@
+// -------- Gerador de usuarios -------- //
+// 1. Captura de elementos
+const btnUsuario = document.getElementById('btn-usuario')
+const usuariosContainer = document.getElementById('usuarios-container')
+const helperTextUsuario = document.getElementById('helper-text-usuario')
+
+// 2. Funções
+function gerarUsuario(){
+   /*MANEIRA DIDÁTICA
+    let resposta = fetch(' https://random-data-api.com/api/v2/users')
+    console.log(resposta) 
+
+    let tratamentoDeResposta = resposta.then((res)=>{
+console.log(res)
+//aqui estamos convertendo JSON para OBJETO. é o mesmo que usar o JSON.parse, só que para usar o Fetch, temos que escrever do jeito a baixo
+return res.json()
+    })
+
+    tratamentoDeResposta.then((data)=>{
+        console.log(data)
+    })
+    */
+    
+    //MANEIRA SIMPLIFICADA DE FAZER A MESMA COISA QUE FEZ O CÓDIGO ACIMA
+    fetch(' https://random-data-api.com/api/v2/users')
+    .then((res)=> res.json())
+    .then((data)=> {
+        const usuario = document.createElement('div')
+            usuario.innerHTML = `
+            <figure>
+            <img src="${data.avatar}" />
+            </figure>
+            <figcaption>
+            <p><strong>Username:</strong>${" "+data.first_name}, ${data.employment.title}.</p>
+            </figcaption>
+            <button class="remover">Remover</button>
+            
+            `
+            
+            usuario.querySelector('.remover') 
+            .addEventListener('click',()=>{
+                usuariosContainer.removeChild(usuario)
+            })
+            usuariosContainer.appendChild(usuario)
+            console.log(data)
+        
+    })
+}
+
+// 3. Eventos
+btnUsuario.addEventListener('click', gerarUsuario)
+
+
+
+   
